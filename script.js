d4fe926beaeb1fe1aa4f3ef0979c03a3a9d00547
mrluckycrack47-blip <mr.lucky.crack47@gmail.com>
2025-11-25 10:14:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function goHome(){
+  document.getElementById('productView').style.display = 'none';
+  document.getElementById('checkout').style.display = 'none';
+  document.getElementById('mainPage').style.display = 'block';
+  window.scrollTo(0, 0);
+}
